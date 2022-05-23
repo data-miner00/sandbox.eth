2e22d4cc0e05a73890768e84783f54198c17c1bc
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-23 13:59:11 +0800

    test: Added crud and test

--- migrations/1652670427_sample.js
+++ migrations/1652670427_sample.js
@@ -1,7 +1,9 @@
 const Sample = artifacts.require("Sample");
 const Storage = artifacts.require("Storage");
+const Crud = artifacts.require("Crud");
 
 module.exports = function(_deployer) {
   _deployer.deploy(Sample);
   _deployer.deploy(Storage);
+  _deployer.deploy(Crud);
 };
